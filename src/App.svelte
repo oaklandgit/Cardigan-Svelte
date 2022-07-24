604044f52f4e648dd8b74373a5a2d323c15a6ae2
Larry S <muppethead@mac.com>
2022-07-23 18:18:55 -0700

    Initial svelt stuff and playing with HyperCard-ish backgrounds.

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Bak from "./lib/Bak.svelte";
+  import Palette from "./lib/Palette.svelte";
+</script>
+
+<Palette>
+  <p>TITLE</p>
+  <p>BODY</p>
+</Palette>
+
+<Bak pattern={1}>
+  <h1>Hello</h1>
+  <p>The rain in spain</p>
+</Bak>
+
+<Bak pattern={2}>
+  <h1>Hello</h1>
+  <p>The rain in spain</p>
+</Bak>
+
+<Bak pattern={3}>
+  <h1>Hello</h1>
+  <p>The rain in spain</p>
+</Bak>
+
+<style>
+</style>
---
+++ src/lib/Palette.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Draggable from "./Draggable.svelte";
+</script>
+
+<Draggable>
+    <div>
+        <slot />
+    </div>
+</Draggable>
+
+<style>
+    div {
+        background-color: white;
+        padding: 12px;
+        border: 2px solid black;
+        box-shadow: 2px 2px 2px black;
+    }
+</style>
